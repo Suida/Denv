a3f4e019ee04ab103d433e7dfc50dbf40ea43ae5
Suida <suidar@foxmail.com>
2020-03-15 22:28:27 +0800

    Update settings

---
+++ modules/keymappings/sync.bat
@@ -0,0 +1,6 @@
+scp sync.bat hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/sync.bat
+scp aura.ahk hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/aura.ahk
+scp profiles.json.win-terminal hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/profiles.json.win-terminal
+scp keybindings.json.win-vscode hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/keybindings.json.win-vscode
+scp settings.json.win-vscode hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/settings.json.win-vscode
+
