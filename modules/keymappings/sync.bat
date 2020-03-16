fde900d3f8da5fe48c1b88bc12445e447e250fee
Suida <suidar@foxmail.com>
2020-03-16 21:12:38 +0800

    Update sync app

--- modules/keymappings/sync.bat
+++ modules/keymappings/sync.bat
@@ -1,6 +1,9 @@
 scp sync.bat hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/sync.bat
 scp aura.ahk hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/aura.ahk
+
+cp C:\Users\You\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json profiles.json.win-terminal
 scp profiles.json.win-terminal hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/profiles.json.win-terminal
+
 scp keybindings.json.win-vscode hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/keybindings.json.win-vscode
 scp settings.json.win-vscode hugh@192.168.101.5:~/workspace/Denv/modules/keymappings/settings.json.win-vscode
 
